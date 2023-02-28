b593192d8083f03b9a9da780913304e82360e4cf
Priyaraj12 <raj.t@vastuhfc.com>
2023-02-28 19:19:36 +0530

    Vastu icici colending insert query batch 1. Rest need to be continued

---
+++ lender_vastu_icici_citydivision.sql
@@ -0,0 +1,139 @@
+select max(cityDivisionlenderId) from ref_lender_citydivision where lenderId=11;
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1024,'Patan','Gujarat','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1025,'Vadodara','Gujarat','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1026,'Navi Mumbai','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1027,'Pune','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1028,'Ahmednagar','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1029,'Thane','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1030,'Nasik','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1031,'Shrirampur','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1032,'Bastar','CHATTISGARH','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1033,'Aska','ODISHA','11',1,now(),now(),'Vastu','Vastu');
+
+-- TamilNadu and Kerala
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1034,'Tirupur','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1035,'Erode','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1036,'Coimbatore','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1037,'Pollachi','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1038,'Nilgiris','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+-- Kerala
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1039,'Palghat','Kerala','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1040,'Trichur','Kerala','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1041,'Alwaye','Kerala','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1042,'Kottayam','Kerala','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1043,'Sawaimadhopur','Rajasthan','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1044,'Nagpur','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1045,'Wardha','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1046,'Tiruchirapalli','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+
+-- Madhya Pradesh
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1047,'Khandwa','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1048,'Indore','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1049,'Ujjain','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1050,'Ratlam','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1051,'Mandsaur','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1052,'Chhindwara','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1053,'Sehore','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1054,'Sagar','Madhya Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+-- CHATTISGARH and Telengana
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1055,'Raigarh','CHATTISGARH','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1056,'Peddapalli','TELANGANA','11',1,now(),now(),'Vastu','Vastu');
+
+-- Maharashtra and Bihar
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1057,'Chandrapur','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1058,'Akola','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1059,'Buldana','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1060,'Amaravati','Maharashtra','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1061,'Aurangabad(Bihar)','Bihar','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1062,'Chikodi','KARNATAKA','11',1,now(),now(),'Vastu','Vastu');
+-- Andhra Pradesh
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1063,'Kakinada','Andhra Pradesh','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1064,'Rajahmundry','Andhra Pradesh','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1065,'Amalapuram','Andhra Pradesh','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1066,'Tadepalligudem','Andhra Pradesh','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1067,'Bhimavaram','Andhra Pradesh','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1068,'Eluru','Andhra Pradesh','11',1,now(),now(),'Vastu','Vastu');
+-- Karnataka
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1069,'Bengaluru','Karnataka','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1070,'Channapatna','Karnataka','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1071,'Kolar','Karnataka','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1072,'Arakkonam','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1073,'Krishnagiri','Tamil Nadu','11',1,now(),now(),'Vastu','Vastu');
+
+INSERT INTO `staging`.`ref_lender_citydivision` (`cityDivisionlenderId`, `cityDivision`,`state`,`lenderid`,`activestatus`,`createdon`,`updatedon`,`createdby`,`updatedby`) 
+VALUES (1074,'Eluru','Andhra Pradesh','11',1,now(),now(),'Vastu','Vastu');
